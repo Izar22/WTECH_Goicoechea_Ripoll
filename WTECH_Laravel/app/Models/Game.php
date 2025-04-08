6ba5fc6c77c3cf8b19af1982ff3ef1a64891f31a
goicoechea.147720 <goicoechea.147720@e.unavarra.es>
2025-04-08 17:08:33 +0200

    product page and filtering

---
+++ WTECH_Laravel/app/Models/Game.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    protected $fillable = [
+        'title',
+        'release_date',
+        'publisher_name',
+        'price',
+        'platform',
+        'region',
+        'genre',
+        'description',
+    ];
+}
