48c9335cd9490a573bd7bb04ffa33caf9720238c
goicoechea.147720 <goicoechea.147720@e.unavarra.es>
2025-04-15 11:49:01 +0200

    modifications on database

--- WTECH_Laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ WTECH_Laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            //$table->unsignedBigInteger('shipping_details_id')->nullable();
+            //$table->unsignedBigInteger('payment_details_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            //$table->foreign('shipping_details_id')->references('id')->on('shipping_details')->onDelete('cascade');
+            //$table->foreign('payment_details_id')->references('id')->on('payment_details')->onDelete('cascade');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
